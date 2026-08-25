98c28a4c52c3d0d17261d4ccb0f6a44064dd10b6
Nitin Purohit <nitinpurohit412@gmail.com>
2026-08-25 14:23:53 +0530

    exploring get, post and delete

--- homework.md
+++ homework.md
@@ -1,2 +1,4 @@
 - what are dependencies
 - difference b/w caret and tilde - (^ vs ~)
+
+- what is diffrence b/w package.json and lock.json. and why we dont put lock.json in gitignore.
--- src/app.js
+++ src/app.js
@@ -2,17 +2,26 @@ const express = require("express");
 
 const app = express()
 
-app.use("/hello", (req, res) =>{
-    res.send("Hello")
-});
+app.get("/user", (req, res)=>{
+    res.send({ firstName : "Nitin", lastName : "Purohit"})
+})
+
+app.post("/user", (req, res)=>{
+    //saving data to DB
+    res.send("Data successfully saved to the Database")
+})
+
+app.delete("/user", (req, res)=>{
+    res.send("Deleted successfully")
+})
 
 app.use("/test", (req, res) =>{
     res.send("Namaste test")
 });
 
-app.use("/", (req, res) =>{
-    res.send("hello hello hello !!!")
-});
+// app.use("/", (req, res) =>{
+//     res.send("hello hello hello !!!")
+// });
 
 app.listen(7777, ()=>{
     console.log("server is successfully listen on port 7777")
