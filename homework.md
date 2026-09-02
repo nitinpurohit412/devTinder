9e9da12644daba6d00cd68df7cca1c9c66ee98f8
Nitin Purohit <nitinpurohit412@gmail.com>
2026-09-02 12:40:36 +0530

    About models

--- homework.md
+++ homework.md
@@ -4,3 +4,6 @@
 - what is diffrence b/w package.json and lock.json. and why we dont put lock.json in gitignore.
 - Read more about middlewears.
 - Diffrence blw app.use and all.
+
+- Diffrence blw json and JS object.
+- Diff blw patch and put
--- src/app.js
+++ src/app.js
@@ -2,14 +2,13 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const user = require("./models/user");
+
+app.use(express.json())
 
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Ejji",
-    lastName: "Date",
-    emailId: "ejji@datte.com",
-    password: "datte@123",
-  });
+
+  const user = new User(req.body)
 
   try {
     await user.save();
@@ -18,6 +17,59 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("Error saving the user" + err.message);
   }
 });
+
+//*  Get User by email.
+app.get("/user", async(req, res)=>{
+  const userEmail = req.body.emailId
+
+ try{
+  const users = await User.find({emailId : userEmail})
+  if(users.length === 0) {
+    res.status(404).send("User not found")
+  } else{
+    res.send(users)
+  }
+ }  catch (err){
+  res.status(400).send("Something went wrong")
+ }
+})
+
+
+//* Feed API - GET /feed  - get all the user from the databse
+app.get("/feed" , async (req, res)=>{
+  try {
+    const users = await User.find({})
+    res.send(users)
+  } catch (err) {
+     res.status(400).send("Something went wrong")
+  }
+})
+
+//* Delete user from the databse
+app.delete("/user" , async (req, res) =>{
+ const userId = req.body.userId
+  try {
+    const user = await User.findByIdAndDelete(userId)
+    res.send("User sucessfully deleted.")
+  } catch (err) {
+    res.status(400).send("Something went wrong")
+  }
+})
+
+
+//* Update the user 
+app.patch("/user" , async (req ,res) =>{
+  const userId = req.body.userId
+  const data = req.body
+  try {
+    const user = await User.findByIdAndUpdate(userId, data)
+    console.log(user)
+    res.send("User successfully updated")
+  } catch (err) {
+    res.status(400).send("Something went wrong")
+  }
+})
+
 
 connectDB()
   .then(() => {
