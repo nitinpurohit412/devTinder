fd3e6932cd70ae1e77408a97c6ff0e35644c8216
Nitin Purohit <nitinpurohit412@gmail.com>
2026-09-03 11:41:33 +0530

    schema types

--- src/app.js
+++ src/app.js
@@ -62,7 +62,10 @@ app.patch("/user" , async (req ,res) =>{
   const userId = req.body.userId
   const data = req.body
   try {
-    const user = await User.findByIdAndUpdate(userId, data)
+    const user = await User.findByIdAndUpdate({ _id : userId}, data, {
+      returnDocument : "after",
+      runValidators : true,
+    })
     console.log(user)
     res.send("User successfully updated")
   } catch (err) {
--- src/models/user.js
+++ src/models/user.js
@@ -3,22 +3,40 @@ const mongoose = require("mongoose")
 const userSchema = new mongoose.Schema({
     firstName : {
         type : String,
+        required : true,
     },
     lastName : {
         type : String,
     },
     emailId :{
         type : String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        trim : true
     },
     password : {
         type : String,
+        required : true,
     },
     age : {
         type : Number,
+        min : 18,
     },
     gender : {
         type : String,
+        validate(value){
+            if(!["male", "female", "others"].includes(value)){
+                throw new Error("Invalid Gender")
+            }
+        }
+    },
+    skills : {
+        type : [String],
     }
+}, 
+{
+    timestamps : true,
 })
 
 module.exports = mongoose.model("User" , userSchema)
